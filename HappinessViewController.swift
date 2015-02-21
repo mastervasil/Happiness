532df04d4444d766a8e321de753f3d314bfc100e
mastervasil <mastervasil@gmail.com>
2015-02-21 21:45:47 +0300

    Create face

---
+++ HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Vasil on 21.02.15.
+//  Copyright (c) 2015 vasil. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+    
+}
